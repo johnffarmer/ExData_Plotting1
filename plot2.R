ab36bac6a5a3968be8d4a6118443f40f2f019d8c
JPC <johnfredfarmer@gmail.com>
2015-05-10 14:28:29 -0700

    with plot2

---
+++ plot2.R
@@ -0,0 +1,9 @@
+household_data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?",stringsAsFactors=FALSE)
+household_data$DateTime <- paste(household_data$Date, household_data$Time)
+household_data$DateTime <- strptime(household_data$DateTime, "%d/%m/%Y %H:%M:%S")
+
+filtered_data <- household_data[household_data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+png("plot2.png")
+plot(filtered_data$DateTime, filtered_data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
